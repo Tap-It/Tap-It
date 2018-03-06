bf76aac6b414fc0a33a06cf81ca59798392dcb36
Jonathan Oliveira <jon.brasileiro@gmail.com>
2018-03-06 16:53:08 -0500

    basic connectivity with hosting racing condition solved

---
+++ Tap-It/ColorServiceManagerDelegate.swift
@@ -0,0 +1,141 @@
+import Foundation
+import MultipeerConnectivity
+
+protocol ColorServiceManagerDelegate {
+	func connectedDevicesChanged(manager : ColorServiceManager, connectedDevices: [String])
+	func colorChanged(manager : ColorServiceManager, colorString: String)
+}
+
+class ColorServiceManager: NSObject {
+	
+	private let ClickServiceType = "example-click"
+	private let myPeerId = MCPeerID(displayName: UIDevice.current.name)
+	private let service: MCNearbyServiceAdvertiser
+	private let browser: MCNearbyServiceBrowser
+	var sessionInitTime = Date()
+	var delegate: ColorServiceManagerDelegate?
+	var isHost = false
+	
+	lazy var session: MCSession = {
+		let session = MCSession(peer: self.myPeerId, securityIdentity: nil, encryptionPreference: .required)
+		session.delegate = self
+		return session
+	}()
+	
+	override init() {
+		self.service = MCNearbyServiceAdvertiser(peer: self.myPeerId, discoveryInfo: nil, serviceType: self.ClickServiceType)
+		self.browser = MCNearbyServiceBrowser(peer: self.myPeerId, serviceType: self.ClickServiceType)
+		super.init()
+		self.service.delegate = self
+		self.service.startAdvertisingPeer()
+		self.browser.delegate = self
+		self.browser.startBrowsingForPeers()
+	}
+	
+	deinit {
+		self.service.stopAdvertisingPeer()
+		self.browser.stopBrowsingForPeers()
+	}
+	
+	@objc private func startAdvertising() {
+		if !self.isHost {
+			self.isHost = true
+			self.service.delegate = self
+			self.service.startAdvertisingPeer()
+		}
+	}
+	
+	private func startBrowsing() {
+		self.browser.delegate = self
+		self.browser.startBrowsingForPeers()
+	}
+	
+	func send(colorName : String) {
+		NSLog("sendColor: \(colorName) to \(session.connectedPeers.count) peers")
+		if session.connectedPeers.count > 0 {
+			do {
+				try self.session.send(colorName.data(using: .utf8)!, toPeers: session.connectedPeers, with: .reliable)
+			}
+			catch let error {
+				NSLog("Error for sending: \(error)")
+			}
+		}
+	}
+	
+}
+
+extension ColorServiceManager: MCNearbyServiceAdvertiserDelegate {
+	
+	func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
+		print("didNotStartAdvertisingPeer \(error)")
+	}
+	
+	//	This code accepts all incoming connections automatically.
+	//	To keep sessions private the user should be notified and asked to confirm incoming connections. This can be implemented using the MCAdvertiserAssistant classes.
+	func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+		print("didReceiveInvitationFromPeer \(peerID)")
+		var hostTimer = TimeInterval()
+		if let hostContext = context {
+			hostTimer = hostContext.withUnsafeBytes { (ptr: UnsafePointer<TimeInterval>) -> TimeInterval in
+				return ptr.pointee
+			}
+		}
+		let peerRunningTime = -sessionInitTime.timeIntervalSinceNow
+		let isPeerOlder = (hostTimer > peerRunningTime)
+		invitationHandler(isPeerOlder, self.session)
+		if isPeerOlder {
+			print(#line, "accepting invitation from \(peerID)")
+		}
+		invitationHandler(true, self.session)
+	}
+}
+
+extension ColorServiceManager: MCNearbyServiceBrowserDelegate {
+	
+	func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+		print("lostPeer \(peerID)")
+	}
+	
+	//	This code invites any peer automatically. The MCBrowserViewController class could be used to scan for peers and invite them manually.
+	func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+		print("foundPeer \(peerID)")
+		print("invitePeer \(peerID)")
+		let toUseSession = self.session
+		var runningTime = -sessionInitTime.timeIntervalSinceNow
+		let data = Data(buffer: UnsafeBufferPointer(start: &runningTime, count: 1))
+		browser.invitePeer(peerID, to: toUseSession, withContext: data, timeout: 20)
+	}
+	
+	func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
+		print("didNotStartBrowsingForPeers \(error)")
+	}
+	
+}
+
+extension ColorServiceManager: MCSessionDelegate {
+	
+	func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+		print("peer: \(peerID), didChangeState: \(state)")
+		self.delegate?.connectedDevicesChanged(manager: self, connectedDevices:
+			session.connectedPeers.map{$0.displayName})
+	}
+	
+	func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+		print("didReceiveData: \(data)")
+		let str = String(data: data, encoding: .utf8)!
+		self.delegate?.colorChanged(manager: self, colorString: str)
+	}
+	
+	func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+		print("didReceiveStream")
+	}
+	
+	func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+		print("didFinishReceivingResourceWithName")
+	}
+	
+	func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+		print("didFinishReceivingResourceWithName")
+	}
+}
+
--- Tap-It/ViewController.swift
+++ Tap-It/ViewController.swift
@@ -1,25 +1,88 @@
-//
-//  ViewController.swift
-//  Tap-It
-//
-//  Created by Jonathan Oliveira on 06/03/18.
-//  Copyright © 2018 Jonathan Oliveira. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	var data = [String]()
+	let colorService = ColorServiceManager()
+	
+	@IBOutlet weak var tableView: UITableView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		colorService.delegate = self
 		// Do any additional setup after loading the view, typically from a nib.
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	@IBAction func yellowTapped() {
+		self.change(color: .yellow)
+		colorService.send(colorName: "yellow")
+	}
+	
+	@IBAction func redTapped() {
+		self.change(color: .red)
+		colorService.send(colorName: "red")
+	}
+	
+	func change(color : UIColor) {
+		UIView.animate(withDuration: 0.2) {
+			self.view.backgroundColor = color
+		}
+	}
+	
 }
 
+extension ViewController : ColorServiceManagerDelegate {
+	
+	func connectedDevicesChanged(manager: ColorServiceManager, connectedDevices: [String]) {
+		OperationQueue.main.addOperation {
+			self.data = connectedDevices
+			self.tableView.reloadData()
+		}
+	}
+	
+	func colorChanged(manager: ColorServiceManager, colorString: String) {
+		OperationQueue.main.addOperation {
+			switch colorString {
+			case "red":
+				self.change(color: .red)
+			case "yellow":
+				self.change(color: .yellow)
+			default:
+				NSLog("%@", "Unknown color value received: \(colorString)")
+			}
+		}
+	}
+	
+}
+
+extension ViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return self.data.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let selected = self.data[indexPath.row]
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		cell.textLabel?.text = selected
+		return cell
+	}
+}
+
+extension CGFloat {
+	static func random() -> CGFloat {
+		return CGFloat(arc4random()) / CGFloat(UInt32.max)
+	}
+}
+
+extension UIColor {
+	static func random() -> UIColor {
+		return UIColor(red:   .random(),
+					   green: .random(),
+					   blue:  .random(),
+					   alpha: 1.0)
+	}
+}

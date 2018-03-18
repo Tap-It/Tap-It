9a52648f539e6443d68a8c85dde79703e0432cdb
Fernando Zanei <fernandozanei@icloud.com>
2018-03-18 10:55:44 -0400

    cards and score layout fixed

--- Tap-It/Controller/FigureViewController.swift
+++ Tap-It/Controller/FigureViewController.swift
@@ -2,15 +2,18 @@ import UIKit
 
 class FigureViewController: UIViewController, UIGestureRecognizerDelegate {
     
-    var gameManager:FigureGameManager?
-	var topCard: UIImageView!
-	var bottomCard: UIImageView!
+    var layouted = false
+    let bottomCard = UIImageView()
+    let topCard = UIImageView()
+    let stackView = UIStackView()
     var topScore = [PlayerScore]()
+    var deckLabel:UILabel!
+    var playerCardLabel:UILabel!
+
+    var gameManager:FigureGameManager?
 	let imageSizeRatio = CGFloat(65.0 / 300.0)
 	var imageSize:CGSize!
 	var hasLayout = false
-	var deckLabel:UILabel!
-	var playerCardLabel:UILabel!
 	var numOfPeers: Int!
 	var card:Card? {
 		didSet {
@@ -29,89 +32,24 @@ class FigureViewController: UIViewController, UIGestureRecognizerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // BACKGROUND GRADIENT
-        
+		
         let gradientLayer = CAGradientLayer()
         let topColor = UIColor(red: 250.0/255.0, green: 215.0/255.0, blue: 95.0/255.0, alpha: 1.0).cgColor
         let bottomColor = UIColor(red: 245.0/255.0, green: 125.0/255.0, blue: 55.0/255.0, alpha: 1.0).cgColor
         gradientLayer.colors = [topColor, bottomColor]
         gradientLayer.frame = view.frame
         view.layer.insertSublayer(gradientLayer, at: 0)
-        
-        // BOTTOM CARD
-        
-        bottomCard = UIImageView()
-        bottomCard.image = UIImage(named: "card_3")
-        bottomCard.translatesAutoresizingMaskIntoConstraints = false
-        bottomCard.isUserInteractionEnabled = true
-        view.addSubview(bottomCard)
-        
-        bottomCard.leftAnchor.constraint(greaterThanOrEqualTo: view.leftAnchor, constant: 8).isActive = true
-        bottomCard.rightAnchor.constraint(lessThanOrEqualTo: view.rightAnchor, constant: -8).isActive = true
-        bottomCard.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8).isActive = true
-        bottomCard.widthAnchor.constraint(equalTo: bottomCard.heightAnchor, constant: 0).isActive = true
-        bottomCard.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
-        
-        // top CARD
-        
-        topCard = UIImageView()
-        topCard.image = UIImage(named: "card_3")
-        topCard.translatesAutoresizingMaskIntoConstraints = false
-        topCard.isUserInteractionEnabled = true
-        view.addSubview(topCard)
-        topCard.centerXAnchor.constraint(equalTo: bottomCard.centerXAnchor, constant: 0).isActive = true
-        topCard.widthAnchor.constraint(equalTo: bottomCard.widthAnchor, multiplier: 0.8).isActive = true
-        topCard.heightAnchor.constraint(equalTo: bottomCard.heightAnchor, multiplier: 0.8).isActive = true
-        topCard.bottomAnchor.constraint(equalTo: bottomCard.topAnchor, constant: -16).isActive = true
-		
 
-		deckLabel = UILabel()
-		deckLabel.translatesAutoresizingMaskIntoConstraints = false
-		self.view.addSubview(deckLabel)
-		deckLabel.bottomAnchor.constraint(equalTo: topCard.bottomAnchor, constant: 0.0).isActive = true
-		deckLabel.rightAnchor.constraint(equalTo: topCard.leftAnchor, constant: 0.0).isActive = true
-		deckLabel.leftAnchor.constraint(greaterThanOrEqualTo: view.leftAnchor, constant: 8.0).isActive = true
-		deckLabel.text = String("0")
-
-		playerCardLabel = UILabel()
-		playerCardLabel.translatesAutoresizingMaskIntoConstraints = false
-		self.view.addSubview(playerCardLabel)
-		playerCardLabel.bottomAnchor.constraint(equalTo: bottomCard.bottomAnchor, constant: 8.0).isActive = true
-		playerCardLabel.rightAnchor.constraint(equalTo: bottomCard.leftAnchor, constant: 0.0).isActive = true
-		playerCardLabel.leftAnchor.constraint(greaterThanOrEqualTo: view.leftAnchor, constant: 8.0).isActive = true
-		playerCardLabel.text = String("0")
-		
-		let stackview = UIStackView()
-		stackview.axis = .horizontal
-		stackview.distribution = .equalSpacing
-		
-		var countTopPlayers = 0
-		while countTopPlayers < 3 {
-			if numOfPeers > countTopPlayers {
-				if let objects = Bundle.main.loadNibNamed("PlayerScore", owner: self, options: nil), let scoreview = objects.first as? PlayerScore {
-					self.topScore.append(scoreview)
-					stackview.addArrangedSubview(scoreview)
-				}
-			}
-			countTopPlayers += 1
-		}
-//		stackview.addArrangedSubview(topScore)
-		
-		stackview.translatesAutoresizingMaskIntoConstraints = false
-		view.addSubview(stackview)
-		
-		stackview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
-		stackview.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 32).isActive = true
-		stackview.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -32).isActive = true
-		stackview.bottomAnchor.constraint(equalTo: topCard.topAnchor, constant: -8).isActive = true
-		stackview.heightAnchor.constraint(equalTo: bottomCard.heightAnchor, multiplier: 0.20).isActive = true
-		
         gameManager!.delegate = self
 		gameManager!.shouldStartGame()
     }
 	
 	override func viewDidLayoutSubviews() {
+        
+        
+        loadLayout()
+        
+        
 		if self.card != nil && self.hasLayout == false {
 			self.setupCard(card!, isDeck: false)
 		}
@@ -198,17 +136,95 @@ class FigureViewController: UIViewController, UIGestureRecognizerDelegate {
     }
 }
 
+extension FigureViewController {
+    private func loadLayout() {
+        if !layouted {
+            let frame = view.safeAreaLayoutGuide.layoutFrame
+            let safeTop = view.safeAreaInsets.top
+            
+            // BOTTOM CARD - PLAYER
+            
+            bottomCard.image = UIImage(named: "card_1")
+            
+            var tempHeight = CGFloat(0.0)
+            var tempWidth = CGFloat(0.0)
+            
+            tempHeight = frame.height * 0.47
+            tempWidth = tempHeight
+            
+            bottomCard.frame = CGRect(x: (frame.width/2)-(tempWidth/2), y: frame.height+safeTop-tempHeight-8, width: tempWidth, height: tempHeight)
+            
+            view.addSubview(bottomCard)
+            
+            // TOP CARD - DECK
+            
+            topCard.image = UIImage(named: "card_3")
+            
+            tempHeight = frame.height * 0.37
+            tempWidth = tempHeight
+            
+            topCard.frame = CGRect(x: (frame.width/2)-(tempWidth/2), y: bottomCard.frame.minY-tempHeight-8, width: tempWidth, height: tempHeight)
+            
+            view.addSubview(topCard)
+            
+            // STACK VIEW - SCORE
+            
+            stackView.axis = .horizontal
+            stackView.distribution = .equalSpacing
+            
+            tempHeight = topCard.frame.minY-safeTop-16
+            tempWidth = frame.width-32
+            
+            stackView.frame = CGRect(x: (frame.width/2)-(tempWidth/2), y: safeTop+8, width: tempWidth, height: tempHeight)
+            
+            view.addSubview(stackView)
+            
+            
+            deckLabel = UILabel()
+            deckLabel.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(deckLabel)
+            deckLabel.bottomAnchor.constraint(equalTo: topCard.bottomAnchor, constant: 0.0).isActive = true
+            deckLabel.rightAnchor.constraint(equalTo: topCard.leftAnchor, constant: 0.0).isActive = true
+            deckLabel.leftAnchor.constraint(greaterThanOrEqualTo: view.leftAnchor, constant: 8.0).isActive = true
+            deckLabel.text = String("0")
+            
+            playerCardLabel = UILabel()
+            playerCardLabel.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(playerCardLabel)
+            playerCardLabel.bottomAnchor.constraint(equalTo: bottomCard.bottomAnchor, constant: 8.0).isActive = true
+            playerCardLabel.rightAnchor.constraint(equalTo: bottomCard.leftAnchor, constant: 0.0).isActive = true
+            playerCardLabel.leftAnchor.constraint(greaterThanOrEqualTo: view.leftAnchor, constant: 8.0).isActive = true
+            playerCardLabel.text = String("0")
+            
+            var countTopPlayers = 0
+            while countTopPlayers < 3 {
+                if numOfPeers > countTopPlayers {
+                    if let objects = Bundle.main.loadNibNamed("PlayerScore", owner: self, options: nil), let scoreview = objects.first as? PlayerScore {
+                        self.topScore.append(scoreview)
+                        stackView.addArrangedSubview(scoreview)
+                    }
+                }
+                countTopPlayers += 1
+            }
+
+            
+            layouted = true
+        }
+        
+    }
+}
+
 extension FigureViewController : FigureProtocol {
 	
 	func updateDeckCount(_ total: Int) {
 		DispatchQueue.main.async {
-			self.deckLabel.text = String(total)
+//            self.deckLabel.text = String(total)
 		}
 	}
 	
 	func updatePlayerScore(_ score: Int) {
 		DispatchQueue.main.async {
-			self.playerCardLabel.text = String(score)
+//            self.playerCardLabel.text = String(score)
 		}
 	}
 	

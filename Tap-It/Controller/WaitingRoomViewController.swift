5371da308229fe634db2786381752c5404ae8f49
Jonathan Oliveira <jon.brasileiro@gmail.com>
2018-03-18 23:58:35 -0400

    UI of waiting room and splash screen created

--- Tap-It/Controller/WaitingRoomViewController.swift
+++ Tap-It/Controller/WaitingRoomViewController.swift
@@ -2,29 +2,100 @@ import UIKit
 
 class WaitingRoomViewController: UIViewController {
 
-	@IBOutlet weak var tableView: UITableView!
-	@IBOutlet weak var playButton: UIButton!
-//	var peersTuple:[(Int, String)]!
+	@IBOutlet weak var startButton: UIButton!
+	@IBOutlet weak var titleLabel: UILabel!
+	var playersSlots = [UIView]()
+	var playersViews = [UIView]()
+	var hasLayoutSlots = false
+	var chosenImages = Set<Int>()
+
 	var playerName:String! {
 		didSet {
 			self.initializeManager()
-			self.data = [playerName]
+			self.players = [(1,playerName)]
 		}
 	}
 	var manager = FigureGameManager()
 	
-	var data = [String]() {
+	var players = [(Int, String)]() {
+		willSet {
+			for view in self.playersViews {
+				view.removeFromSuperview()
+			}
+		}
 		didSet {
-			self.tableView.reloadData()
+			for i in 0..<self.players.count {
+				self.addPlayerToView(name: self.players[i].1, imageNum:self.players[i].0, slot: i)
+			}
 		}
 	}
 
 	override func viewDidLoad() {
         super.viewDidLoad()
+		let gradientLayer = CAGradientLayer()
+		let topColor = UIColor(red: 250.0/255.0, green: 215.0/255.0, blue: 95.0/255.0, alpha: 1.0).cgColor
+		let bottomColor = UIColor(red: 245.0/255.0, green: 125.0/255.0, blue: 55.0/255.0, alpha: 1.0).cgColor
+		gradientLayer.colors = [topColor, bottomColor]
+		gradientLayer.frame = view.frame
+		view.layer.insertSublayer(gradientLayer, at: 0)
     }
 	
 	override func viewDidAppear(_ animated: Bool) {
 		self.generatePlayerName()
+	}
+	
+	override func viewDidLayoutSubviews() {
+		if !self.hasLayoutSlots {
+			self.generatePlayersSlot(numOfPlayers: 8)
+		}
+	}
+	
+	private func addPlayerToView(name:String, imageNum:Int, slot:Int) {
+
+		let playerSlot = self.playersSlots[slot]
+		
+		let playerView = UIView(frame: playerSlot.frame)
+
+		let thumbRatio = CGFloat(0.75)
+		let thumbSize = CGSize(width: playerView.frame.size.height * thumbRatio, height: playerView.frame.size.height * thumbRatio)
+		let thumbOrigin = CGPoint(x: 20.0, y: (playerView.frame.size.height / 2) - (thumbSize.height / 2))
+		let playerThumb = UIImageView(frame: CGRect(origin: thumbOrigin, size: thumbSize))
+		let imageName = "figure_\(imageNum)"
+		playerThumb.image = UIImage(named: imageName)
+		playerThumb.contentMode = .scaleAspectFit
+		
+		let labelSize = CGSize(width: (playerView.frame.width) - (playerThumb.frame.origin.x + playerThumb.frame.size.width + 20.0) * 0.5, height: playerThumb.frame.size.height)
+		let labelOrigin = CGPoint(x: (playerThumb.frame.origin.x + playerThumb.frame.size.width + 20.0), y: playerThumb.frame.origin.y)
+		let playerNameLabel = UILabel(frame: CGRect(origin: labelOrigin, size: labelSize))
+		let customFont = UIFont(name: "American Typewriter", size: 17.0)
+		playerNameLabel.text = name
+		playerNameLabel.font = customFont
+		playerNameLabel.textColor = .black
+		playerNameLabel.numberOfLines = 1
+		
+		playerView.addSubview(playerThumb)
+		playerView.addSubview(playerNameLabel)
+		self.playersViews.append(playerView)
+		self.view.addSubview(playerView)
+	}
+	
+	private func generatePlayersSlot(numOfPlayers:Int) {
+		let startOrigin = CGPoint(x: 0.0, y: (self.titleLabel.frame.origin.y + self.titleLabel.frame.height + 10))
+		let playersViewSize = CGSize(width: self.view.frame.size.width, height: (((self.startButton.frame.origin.y) - (self.titleLabel.frame.origin.y + self.titleLabel.frame.height)) / CGFloat(numOfPlayers)) - 1)
+		var playerViewRect = CGRect(origin: startOrigin, size: playersViewSize)
+		
+		for _ in 0..<numOfPlayers {
+			
+			let playerView = UIView(frame: playerViewRect)
+
+			let playerViewColor = UIColor(red: 245.0/255.0, green: 241.0/255.0, blue: 241.0/255.0, alpha: 1.0)
+			playerView.backgroundColor = playerViewColor
+			
+			self.playersSlots.append(playerView)
+			self.view.addSubview(playerView)
+			playerViewRect.origin.y += playerViewRect.size.height + 1
+		}
+		self.hasLayoutSlots = true
 	}
 	
 	func initializeManager() {
@@ -60,7 +131,7 @@ class WaitingRoomViewController: UIViewController {
 		if segue.identifier == "start" {
 			if let dest = segue.destination as? FigureViewController {
 				dest.gameManager = self.manager
-				dest.numOfPeers = self.data.count
+				dest.numOfPeers = self.players.count
 			}
 		}
 	}
@@ -71,27 +142,11 @@ class WaitingRoomViewController: UIViewController {
 	}
 }
 
-extension WaitingRoomViewController: UITableViewDataSource {
-	
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return self.data.count
-	}
-	
-	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let peer = self.data[indexPath.row]
-		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-		cell.textLabel?.text = peer
-		return cell
-	}
-}
-
 extension WaitingRoomViewController: GameManagerWaitingRoomProtocol {
 	
 	func updatePeersList(_ peers: [(Int, String)]) {
 		DispatchQueue.main.async {
-			self.data = peers.map({ (_,name) -> String in
-				return name
-			})
+			self.players = peers
 		}
 	}
 

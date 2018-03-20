dac0bd31655758a352fb46907b5d9a7d19c613eb
Fernando Zanei <fernandozanei@icloud.com>
2018-03-20 12:04:26 -0400

    load screen

--- Tap-It/Controller/SplashViewController.swift
+++ Tap-It/Controller/SplashViewController.swift
@@ -11,7 +11,28 @@ class SplashViewController: UIViewController {
 		gradientLayer.colors = [topColor, bottomColor]
 		gradientLayer.frame = view.frame
 		view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        var width = view.frame.width*0.6
+        let height = width*0.92
+
+        let logoImage = UIImageView()
+        logoImage.image = UIImage(named: "logo")
+        logoImage.frame = CGRect(x: view.frame.midX-(width/2), y: view.frame.midY-(height/1.5), width: width, height: height)
+        view.addSubview(logoImage)
 		
+        width = width/3
+        
+        let name = UILabel()
+        name.text = "Tap It!"
+        name.font = UIFont(name: "Janda Safe and Sound Solid", size: 60)
+        name.adjustsFontSizeToFitWidth = true
+        name.minimumScaleFactor = 0.1
+        name.numberOfLines = 0
+
+        name.frame = CGRect(x: logoImage.frame.midX-(width/2), y: logoImage.frame.maxY*1.1, width: width, height: width*0.51)
+        
+        view.addSubview(name)
+
 		Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(enterWatingRoom), userInfo: nil, repeats: false)
     }
 
